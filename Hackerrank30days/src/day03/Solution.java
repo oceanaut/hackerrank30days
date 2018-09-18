c74ce47cb380ef08d5be5e5137a1052738d76e51
verona <verona@LAPTOP-GRRA9Q59>
2018-09-18 18:37:51 +0300

    Day 3: Intro to Conditional Statements

---
+++ Hackerrank30days/src/day03/Solution.java
@@ -0,0 +1,32 @@
+package day03;
+//https://www.hackerrank.com/challenges/30-conditional-statements/problem
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        if ( (N % 2) !=0) {
+            System.out.println("Weird");
+        } else if (   (N % 2) ==0  ) {
+            
+            if ( N>=2 && N<=5  ) {
+                System.out.println("Not Weird");
+            } else if ( N>=6 && N<=20 ) {
+                System.out.println("Weird");
+            } else if (N>20 ) {
+                System.out.println("Not Weird");
+            }
+       }
+        
+
+        scanner.close();
+    }
+    
+
+}
